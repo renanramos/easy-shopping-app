26b128f2cadc1486c416a8b085e8e4d99688edc0
Renan Ramos <renan.ramos@inatel.br>
2020-08-25 17:12:27 -0300

    Service de autenticacao

---
+++ src/app/core/service/auth/user-auth-service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Injector } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Login } from '../../models/user/login.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserAuthService extends ApiService<Login>{
+
+  private url: string = '/users/login';
+
+  constructor(injector: Injector) {
+    super(injector);
+  }
+
+
+  login(login: Login): Observable<Login> {
+    return this.post(`${this.url}`, login);
+  }
+}
