49c36ff7827656e4f0381f7c1fab42557d45aa87
Renan Ramos <renan.ramos@inatel.br>
2020-08-25 17:11:37 -0300

    Implementado modulo de login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-
-
+import { AngularMaterialModule } from '../shared/angular-material.module';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginFormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    AngularMaterialModule
+  ],
+  exports: [LoginFormComponent]
 })
 export class LoginModule { }
