26d1a54c3910323aaac0c02990892901a60407a5
Renan Ramos <renan.ramos@inatel.br>
2020-08-25 17:09:15 -0300

    Inicio de implementacao do formulario de login

--- src/app/login/components/login-form/login-form.component.ts
+++ src/app/login/components/login-form/login-form.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CookieService } from 'ngx-cookie-service';
+import { UserAuthService } from '../../../core/service/auth/user-auth-service.service';
+import { Login } from 'src/app/core/models/user/login.model';
+import { tap } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +14,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup = null;
 
-  ngOnInit(): void {
+  constructor(
+    private dialogRef: MatDialogRef<LoginFormComponent>,
+    private formBuider: FormBuilder,
+    private authService: UserAuthService,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.initializeLoginForm();
   }
 
+  initializeLoginForm() {
+    this.loginForm = this.formBuider.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+  
+  async onSubmitLogin() {
+    if (this.loginForm.valid) {
+      await this.authenticateUser(this.loginForm.getRawValue());
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  onCloseDialog() {
+    this.dialogRef.close();
+  }
+
+  async authenticateUser(login: Login) {
+    
+    const receiveUserCredentials = {
+      next: (userCredentials) => {
+        this.dialogRef.close(userCredentials);
+      },
+      error: (response) => {
+        this.loginForm.markAllAsTouched();
+        this.snackBar.open(response.error.message, 'close');
+      }
+    }
+
+    await this.authService.login(login)
+    .pipe(tap(receiveUserCredentials))
+    .toPromise()
+    .then()
+    .catch();
+  }
 }
