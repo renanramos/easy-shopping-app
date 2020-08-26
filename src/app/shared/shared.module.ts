e1140ea5e17304c9e91eb4da54b2c83c4cb1ddc8
Renan Ramos <renan.ramos@inatel.br>
2020-08-26 15:13:15 -0300

    Correcao do arquivo para funcionamento adequado em lazy loading

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AngularMaterialModule } from '../shared/angular-material.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
