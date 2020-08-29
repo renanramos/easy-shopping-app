31c3ea49e80b65b93fefafb8a2e97a089ee618c2
Renan Ramos <renan.ramos@inatel.br>
2020-08-29 15:23:10 -0300

    Arquivo modelo de company

---
+++ src/app/core/models/registration/company.model.ts
@@ -0,0 +1,8 @@
+export class Company {
+  private id: number;
+  private name: string;
+  private registeredNumber: string;
+  private phone: string;
+  private email: string;
+  private password: string;
+}
