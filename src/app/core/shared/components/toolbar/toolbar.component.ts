15579372114213d6fcf1d714c0fd863b8f83843c
Renan Ramos <renan.ramos@inatel.br>
2020-08-29 00:07:04 -0300

    Adicionadas opcoes de menu no toolbar

--- src/app/core/shared/components/toolbar/toolbar.component.ts
+++ src/app/core/shared/components/toolbar/toolbar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { CookieService } from 'ngx-cookie-service';
+import { LoginFormComponent } from 'src/app/login/components/login-form/login-form.component';
+import { UserCredentials } from 'src/app/core/models/user/user-credentials.model';
 
 @Component({
   selector: 'es-toolbar',
@@ -13,9 +17,12 @@ export class ToolbarComponent implements OnInit {
   @Input() showMenuIcon: boolean = true;
   userLoggedName: string = "";
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    public dialog: MatDialog,
+    private cookieService: CookieService) { }
 
   ngOnInit(): void {
+    this.userLoggedName = this.cookieService.get('username');
   }
 
   eventMenuHandler($event) {
@@ -24,5 +31,31 @@ export class ToolbarComponent implements OnInit {
 
   homePage() {
     this.router.navigate(['/']);
+  }
+
+  openLoginDialog() {
+    const dialogRef = this.dialog.open(LoginFormComponent, {
+      disableClose: true,
+      autoFocus: false,
+      panelClass: 'es-dialog'
+    });
+
+    dialogRef.afterClosed().subscribe((userCredentials: UserCredentials) => {
+      if (userCredentials) {
+        this.cookieService.set('role', userCredentials.roles[0]);
+        this.cookieService.set('username', userCredentials.username);
+        this.cookieService.set('token', userCredentials.token);
+        this.userLoggedName = this.cookieService.get('username');
+      }
+    });
+  }
+
+  logout() {
+    this.cookieService.deleteAll();
+    this.userLoggedName = "";
+  }
+
+  onRedirectSignUp() {
+    this.router.navigate(['/registration']);
   }
 }
