8200350cdebaa2f023fab349ff0dcdcdab4d9ea9
Renan Ramos <renan.ramos@inatel.br>
2020-08-29 16:49:02 -0300

    Metodo para salvar nova empresa

--- src/app/main/registration/company-form/company-form.component.ts
+++ src/app/main/registration/company-form/company-form.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { passwordMatcher } from 'src/app/core/shared/validators/password-matcher';
+
+import { passwordMatcher } from '../../../core/shared/validators/password-matcher';
+import { CompanyService } from '../../../core/service/company/company.service';
+import { SnackbarService } from '../../../core/shared/service/snackbar.service';
+import { UtilsService } from '../../../core/shared/utils/utils.service';
+import { Company } from '../../../core/models/registration/company.model';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'es-company-form',
   templateUrl: './company-form.component.html',
-  styleUrls: ['./company-form.component.css']
+  styleUrls: ['./company-form.component.css'],
+  providers: [CompanyService]
 })
 export class CompanyFormComponent implements OnInit {
 
-companyForm: FormGroup;
+  isWaitingResponse: boolean = false;
+  companyForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private companyService: CompanyService,
+    private snackBar: SnackbarService,
+    private utilsService: UtilsService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.createForm();
@@ -31,14 +45,37 @@ companyForm: FormGroup;
     });
   }
 
-  onSubmitCompany() {
+ async onSubmitCompany() {
+    this.isWaitingResponse = true;
     this.companyForm.invalid ?
       this.companyForm.markAllAsTouched() :
-      this.saveCompany();
+      await this.saveCompany();
   }
 
-  saveCompany() {
-    console.log(this.companyForm.getRawValue());
+  async saveCompany() {
+    const company: Company = this.companyForm.getRawValue();
+
+    const receivedCompany = {
+      next: (newCompany: Company) => {
+        newCompany ?
+          this.snackBar.openSnackBar('Empresa criada com sucesso!') :
+          this.snackBar.openSnackBar('A requisição foi efetuada, mas não obtivemos resposta');
+
+        this.isWaitingResponse = false;
+        this.router.navigate(['/']);
+      },
+      error: (response) => {
+        const errorMessage = this.utilsService.handleErrorMessage(response);
+        this.snackBar.openSnackBar(errorMessage);
+        this.isWaitingResponse = false;
+      }
+    }
+
+    await this.companyService.saveCompany(company)
+      .pipe(tap(receivedCompany))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
   }
 
   get name() {
