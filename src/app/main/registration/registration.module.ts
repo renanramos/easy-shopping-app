0129cd2f9f96f71b072d6dbd4fe449d12d15ace8
Renan Ramos <renan.ramos@inatel.br>
2020-08-29 00:31:23 -0300

    Atualizacao de modulo

--- src/app/main/registration/registration.module.ts
+++ src/app/main/registration/registration.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../../shared/shared.module';
 import { AngularMaterialModule } from '../../shared/angular-material.module';
-import { RegistrationComponent } from './registration.component';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { SharedComponentsModule } from '../../core/shared/components/shared-components.module';
 import { CompanyFormComponent } from './company-form/company-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CustomerFormComponent } from './customer-form/customer-form.component';
 
 @NgModule({
   declarations: [
-    RegistrationComponent,
-    CompanyFormComponent
+    CompanyFormComponent,
+    CustomerFormComponent
   ],
   imports: [
     CommonModule,
