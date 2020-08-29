b412f10e92b7e2f70d558a1747b53b1d7edc3114
Renan Ramos <renan.ramos@inatel.br>
2020-08-29 16:50:03 -0300

    UtilsService criado

---
+++ src/app/core/shared/utils/utils.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  constructor() { }
+
+  handleErrorMessage(response: any) {
+    return response.error.errors && response.error.errors.length ? 
+                response.error.errors[0] :
+                response.error.message;
+  }
+}
