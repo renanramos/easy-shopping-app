bc5e8ef8e90fb3058fb6b4c2aa1eb2d3a4bdcb99
Renan Ramos <renan.ramos@inatel.br>
2020-09-01 22:12:29 -0300

    Classe constants com mensagens utilizadas em snackbar

---
+++ src/app/core/shared/constants/constant-messages.ts
@@ -0,0 +1,7 @@
+export class ConstantMessages {
+
+  public static NO_PRODUCT_CATEGORIES_FOUND: string = 'Não há categorias de produtos cadastradas.';
+  public static CANT_GET_PRODUCT_CATEGORIES: string = 'Não foi possível buscar subcategorias.';
+
+  public static SUCCESSFULLY_UPDATED: string = 'Registro atualizado com sucesso!';
+}
