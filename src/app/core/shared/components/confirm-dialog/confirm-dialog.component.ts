c04d5184b4315214cda79e70e76ef56e032bf323
Renan Ramos <renan.ramos@inatel.br>
2020-09-02 18:44:49 -0300

    Componente para alertar o usuario sobre a remocao

---
+++ src/app/core/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'es-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.css']
+})
+export class ConfirmDialogComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+
+}
