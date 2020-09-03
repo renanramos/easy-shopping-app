a0f6dea6a1d2dbbf4cdcfd6c9ae1f493111433b9
Renan Ramos <renan.ramos@inatel.br>
2020-09-02 21:54:02 -0300

    Implementados metodos para alterar ou remover uma empresa

--- src/app/main/company-management/company-list/company-list.component.ts
+++ src/app/main/company-management/company-list/company-list.component.ts
@@ -5,6 +5,10 @@ import { Company } from '../../../core/models/registration/company.model';
 import { SnackbarService } from '../../../core/shared/service/snackbar.service';
 import { UtilsService } from '../../../core/shared/utils/utils.service';
 import { tap } from 'rxjs/operators';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { CompanyDetailComponent } from '../company-detail/company-detail.component';
+import { ConfirmDialogComponent } from 'src/app/core/shared/components/confirm-dialog/confirm-dialog.component';
+import { ConstantMessages } from 'src/app/core/shared/constants/constant-messages';
 
 @Component({
   selector: 'es-company-list',
@@ -17,10 +21,14 @@ export class CompanyListComponent implements OnInit {
   isLoadingCompanies: boolean = false;
   noCompanyFound: boolean = false;
   companies: Company[] = [];
+  
+  dialogRef: MatDialogRef<CompanyDetailComponent>;
+  confirmDialogRef: MatDialogRef<ConfirmDialogComponent>;
 
   constructor(private companyService: CompanyService,
-      private snackBarService: SnackbarService,
-      private utilsService: UtilsService) { }
+    private utilsService: UtilsService,
+    private dialog: MatDialog,
+    private snackBarService: SnackbarService) { }
 
   async ngOnInit() {
     await this.loadCompanies();
@@ -54,6 +62,58 @@ export class CompanyListComponent implements OnInit {
   }
 
   openEditCompany(company: Company) {
-    console.log(company);
+
+    const receivedDialogResponse = {
+      next: (companyUpdated) => {
+        if(companyUpdated) {
+          this.snackBarService.openSnackBar(ConstantMessages.SUCCESSFULLY_UPDATED);
+          this.loadCompanies();
+        }
+      }
+    }
+
+    this.dialogRef = this.dialog.open(CompanyDetailComponent, {
+      data: { company: company },
+      autoFocus: false,
+      disableClose: true,
+      panelClass: 'es-dialog'
+    });
+
+    this.dialogRef.afterClosed().subscribe(receivedDialogResponse);
+  }
+
+  openRemoveCompany(company: Company) {
+    this.confirmDialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: company,
+      autoFocus: false,
+      disableClose: true,
+      panelClass: 'es-small-dialog'
+    });
+
+    this.confirmDialogRef.afterClosed().subscribe((response) => {
+      if (response) {
+        this.removeCompany(company);
+      }
+    });
+  }
+
+  async removeCompany(company: Company) {
+
+    const removedCustomerResponse = {
+      next: () => {
+        this.snackBarService.openSnackBar(ConstantMessages.SUCCESSFULLY_REMOVED, 'close');
+        this.loadCompanies();
+      },
+      error: (error) => {
+        const message = this.utilsService.handleErrorMessage(error);
+        this.snackBarService.openSnackBar(message, 'close');
+      }
+    };
+
+    await this.companyService.removeCompany(company['id'])
+      .pipe(tap(removedCustomerResponse))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
   }
 }
