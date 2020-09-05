c9055028113f4d6e934d87f5f30a6f6cc9a6c7e1
Renan Ramos <renan.ramos@inatel.br>
2020-09-05 20:08:29 -0300

    Tratamento do campo Empresa para ser apresentada somente quando o usuario for admin

--- src/app/main/store-management/store-detail/store-detail.component.ts
+++ src/app/main/store-management/store-detail/store-detail.component.ts
@@ -3,10 +3,12 @@ import { StoreService } from 'src/app/core/service/store/store.service';
 import { CompanyService } from 'src/app/core/service/company/company.service';
 import { UtilsService } from 'src/app/core/shared/utils/utils.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Company } from 'src/app/core/models/registration/company.model';
 import { SnackbarService } from 'src/app/core/shared/service/snackbar.service';
 import { tap } from 'rxjs/operators';
+import { Store } from 'src/app/core/models/store/store.model';
+import { SecurityUserService } from 'src/app/core/service/auth/security-user.service';
 
 @Component({
   selector: 'es-store-detail',
@@ -20,17 +22,27 @@ export class StoreDetailComponent implements OnInit {
   companies: Company[] = [];
   isWaitingResponse: boolean = false;
   isLoadingCompanies: boolean = false;
+  store: Store = new Store();
+  isAdminUser: boolean = false;
 
   constructor(private storeService: StoreService, 
       private companyService: CompanyService,
       private utilsService: UtilsService,
       private formBuilder: FormBuilder,
       private snackBarService: SnackbarService,
+      private securityUserService: SecurityUserService,
+      private dialogRef: MatDialogRef<StoreDetailComponent>,
       @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   async ngOnInit() {
+    this.isAdminUser = this.securityUserService.isAdminUser;
+    this.store = this.data['store'] ? this.data['store'] : new Store();
     this.createForm();
-    await this.loadCompanies();
+    if (this.isAdminUser) {
+      await this.loadCompanies();
+    } else {
+      this.companyId.setValue(this.securityUserService.idUserLoggedIn);
+    }
   }
 
   async loadCompanies() {
@@ -83,7 +95,35 @@ export class StoreDetailComponent implements OnInit {
     return this.storeForm.get('companyId');
   }
 
-  submitStore() {
-    console.log(this.storeForm.getRawValue());
+  async submitStore() {
+    this.storeForm.invalid ?
+      this.storeForm.markAllAsTouched() :
+      await this.saveStore();
+  }
+
+  async saveStore() {
+    this.isWaitingResponse = true;
+
+    const store: Store = this.storeForm.getRawValue();
+
+    const receivedStore = {
+      next: (storeCreated: Store) => {
+        if (storeCreated) {
+          this.dialogRef.close(storeCreated);
+        }
+        this.isWaitingResponse = false;
+      },
+      error: (response) => {
+        const errorMessage = this.utilsService.handleErrorMessage(response);
+        this.snackBarService.openSnackBar(errorMessage, 'close');
+        this.isWaitingResponse = false;
+      }
+    };
+
+    await this.storeService.saveStore(store)
+      .pipe(tap(receivedStore))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
   }
 }
