a3dc2faf51efac8c6e8bd03e14a341d2156a5073
Renan Ramos <renan.ramos@inatel.br>
2020-09-04 23:48:13 -0300

    Adicionado arquivo de constantes de roles do usuario

---
+++ src/app/core/shared/constants/user-roles-constants.ts
@@ -0,0 +1,5 @@
+export class UserRolesConstants {
+  public static ADMINISTRATOR: string = 'ADMINISTRATOR';
+  public static COMPANY: string = 'COMPANY';
+  public static CUSTOMER: string = 'CUSTOMER';
+}
