2f07e9573756212e562a18aa6347abea7bd37169
Renan Ramos <renan.ramos@inatel.br>
2020-09-04 23:45:28 -0300

    novos metodos para obter informacoes do usuario logado

--- src/app/core/service/auth/security-user.service.ts
+++ src/app/core/service/auth/security-user.service.ts
@@ -3,21 +3,20 @@ import { UserCredentials } from '../../models/user/user-credentials.model';
 import { Injector, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthConstants } from '../../shared/constants/auth-constants';
+import { UserRolesConstants } from '../../shared/constants/user-roles-constants';
+import * as jwt_decode from 'jwt-decode';
 
 @Injectable()
 export class SecurityUserService extends ApiService<UserCredentials> {
  
-  private token: string = null;
-
   constructor(
     private router: Router,
-    private injector: Injector) {
+    injector: Injector) {
     super(injector);
   }
 
   setToken(token: string) {
     localStorage.setItem(AuthConstants.AUTH_TOKEN, token);
-    this.token = token;
   }
 
   setUsername(username: string) {
@@ -45,11 +44,11 @@ export class SecurityUserService extends ApiService<UserCredentials> {
     localStorage.removeItem(AuthConstants.AUTH_TOKEN);
     localStorage.removeItem(AuthConstants.USERNAME);
     localStorage.removeItem(AuthConstants.USER_ROLE);
+    localStorage.removeItem(AuthConstants.AUTH_TOKEN);
   }
 
   deleteCookieAndRedirect() {
     this.deleteCookieFromStorage();
-    this.token = null;
     this.router.navigate(['/']);
   }
 
@@ -62,6 +61,16 @@ export class SecurityUserService extends ApiService<UserCredentials> {
   }
 
   getUserLoggedToken() {
-    return localStorage.getItem(AuthConstants.AUTH_TOKEN)
+    return localStorage.getItem(AuthConstants.AUTH_TOKEN);
+  }
+
+  get isAdminUser() {
+    return localStorage.getItem(AuthConstants.USER_ROLE) === UserRolesConstants.ADMINISTRATOR;
+  }
+
+  get idUserLoggedIn() {
+    const token = localStorage.getItem(AuthConstants.AUTH_TOKEN);
+    const decodedToken = jwt_decode(token);
+    return decodedToken['user_id'];
   }
 }
