ec54c82a160f7ae79dfc12f13b6a09ff1be884c1
Renan Ramos <renan.ramos@inatel.br>
2020-09-05 20:05:21 -0300

    Mensagem para cadastro realizado com sucesso

--- src/app/core/shared/constants/constant-messages.ts
+++ src/app/core/shared/constants/constant-messages.ts
@@ -3,6 +3,7 @@ export class ConstantMessages {
   public static NO_PRODUCT_CATEGORIES_FOUND: string = 'Não há categorias de produtos cadastradas.';
   public static CANT_GET_PRODUCT_CATEGORIES: string = 'Não foi possível buscar subcategorias.';
 
+  public static SUCCESSFULLY_CREATED: string = 'Registro incluído com sucesso!';
   public static SUCCESSFULLY_UPDATED: string = 'Registro atualizado com sucesso!';
   public static SUCCESSFULLY_REMOVED: string = 'Registro removido com sucesso!';
 }
