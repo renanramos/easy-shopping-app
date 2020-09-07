c0d4078473b495cc48538e447f6270099d9ee90f
Renan Ramos <renan.ramos@inatel.br>
2020-09-07 02:00:46 -0300

    Incluido outros campos no model de customer

--- src/app/core/models/registration/customer.model.ts
+++ src/app/core/models/registration/customer.model.ts
@@ -1,7 +1,12 @@
+import { CreditCard } from '../credit-card/credit-card.model';
+import { Address } from '../address/address.model';
+
 export class Customer {
   public id: number;
   public name: string;
   public email: string;
   public cpf: string;
   public password: string;
+  public address: Address[];
+  public creditCards: CreditCard[];
 }
