a93fc96e7fee857882eca2a9868449f618d93efd
Renan Ramos <renan.ramos@inatel.br>
2020-09-07 02:03:24 -0300

    Model da entidade address

---
+++ src/app/core/models/address/address.model.ts
@@ -0,0 +1,9 @@
+export class Address {
+  public id: number;
+  public cep: string;
+  public district: string;
+  public number: number;
+  public state: string;
+  public streetName: string;
+  public customerId: number;
+}
