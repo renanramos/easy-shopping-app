0030215a3e2a8db1c27dbf2bfa65903e9b97ffdf
Renan Ramos <renan.ramos@inatel.br>
2020-09-10 12:03:04 -0300

    Configuracao de arquivos e comandos para deploy no heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static('./dist/easy-shopping-app'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: 'dist/easy-shopping-app/'});
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
