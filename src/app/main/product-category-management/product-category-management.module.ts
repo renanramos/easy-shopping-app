a4996615fad9c0fe1afe411bb1ac4c5a2309fcc2
Renan Ramos <renan.ramos@inatel.br>
2020-09-12 23:34:59 -0300

    Import de infinite scroll e ngx-mask

--- src/app/main/product-category-management/product-category-management.module.ts
+++ src/app/main/product-category-management/product-category-management.module.ts
@@ -4,6 +4,11 @@ import { ProductCategoryManagementRoutingModule } from './product-category-manag
 import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
 import { SharedComponentsModule } from 'src/app/core/shared/components/shared-components.module';
 import { ProductCategoryListComponent } from './product-category-list/product-category-list.component';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+import { NgSlimScrollModule } from 'ngx-slimscroll';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
 
 const components = [
   ProductCategoryListComponent
@@ -17,7 +22,10 @@ const components = [
     CommonModule,
     AngularMaterialModule,
     ProductCategoryManagementRoutingModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    NgSlimScrollModule,
+    InfiniteScrollModule,
+    NgxMaskModule.forRoot()
   ],
   exports:[...components]
 })
