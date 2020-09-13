d2224e9f686cd3ea00809d7a2a5577e6fa03a458
Renan Ramos <renan.ramos@inatel.br>
2020-09-12 23:34:14 -0300

    Implementacao da lista de cards de categorias

--- src/app/main/product-category-management/product-category-list/product-category-list.component.ts
+++ src/app/main/product-category-management/product-category-list/product-category-list.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { ProductCategory } from 'src/app/core/models/product-category/product-category.model';
+import { SecurityUserService } from 'src/app/core/service/auth/security-user.service';
+import { ProductCategoryService } from 'src/app/core/service/productCategory/product-category.service';
+import { SnackbarService } from 'src/app/core/shared/service/snackbar.service';
+import { UtilsService } from 'src/app/core/shared/utils/utils.service';
 
 @Component({
-  selector: 'app-product-category-list',
+  selector: 'es-product-category-list',
   templateUrl: './product-category-list.component.html',
-  styleUrls: ['./product-category-list.component.css']
+  styleUrls: ['./product-category-list.component.css'],
+  providers: [ProductCategoryService, SecurityUserService, UtilsService]
 })
 export class ProductCategoryListComponent implements OnInit {
 
-  constructor() { }
+  isLoadingProductCategories: boolean = false;
+  dataNotFound: boolean = false;
+  productCategories: ProductCategory[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private snackBarService: SnackbarService,
+    private productCategoryService: ProductCategoryService) { }
+
+  async ngOnInit() {
+    await this.loadProductCategories();
   }
 
+  async loadProductCategories() {
+    this.isLoadingProductCategories = true;
+    this.dataNotFound = true;
+
+    const productCategoriesReceived = {
+      next: (productCategories: ProductCategory[]) => {
+        if (productCategories.length) {
+          this.productCategories = productCategories;
+          this.dataNotFound = false;
+        }
+        this.isLoadingProductCategories = false;
+      },
+      error: (response) => {
+        this.isLoadingProductCategories = false;
+        this.snackBarService.openSnackBar(response.error.message, 'close');
+      }
+    };
+
+    await this.productCategoryService.getProductCategories()
+      .pipe(tap(productCategoriesReceived))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  onScroll() {
+    console.log('scrolled');
+  }
+
+  openEditCategory(productCategory: ProductCategory) {
+    console.log('onEdit');
+  }
+
+  openRemoveCategory(productCategory: ProductCategory) {
+    console.log('onRemove');
+  }
+
+  onAddNewProductCategory() {
+    console.log('new ProductCategory');
+  }
 }
