c6019f66e45f3159d0d852cb3992ee9698e624aa
Renan Ramos <renan.ramos@inatel.br>
2020-09-14 19:52:10 -0300

    Componente que possui o load spinner

---
+++ src/app/core/shared/components/loader/loader.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'es-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
