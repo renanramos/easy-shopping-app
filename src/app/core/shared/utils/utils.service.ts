c4647a57da8950cb4a7403fc31b572e101459e5b
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 11:38:50 -0300

    Validacao de response antes de obter valores

--- src/app/core/shared/utils/utils.service.ts
+++ src/app/core/shared/utils/utils.service.ts
@@ -8,7 +8,7 @@ export class UtilsService {
   constructor() { }
 
   handleErrorMessage(response: any) {
-    return response.error.errors && response.error.errors.length ? 
+    return response && response.error.errors && response.error.errors.length ? 
                 response.error.errors[0] :
                 response.error.message;
   }
