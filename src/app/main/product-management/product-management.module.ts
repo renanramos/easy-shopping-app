d158f980347cd1294749297e16e46b9cabeb2290
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 23:22:46 -0300

    Adicionados novos componentes

--- src/app/main/product-management/product-management.module.ts
+++ src/app/main/product-management/product-management.module.ts
@@ -1,23 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { NgxMaskModule } from 'ngx-mask';
+import { NgSlimScrollModule } from 'ngx-slimscroll';
+
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductManagementRoutingModule } from './product-management-routing.module';
 import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
 import { SharedComponentsModule } from 'src/app/core/shared/components/shared-components.module';
+import { ProductDeteailComponent } from './product-deteail/product-deteail.component';
 
 const components = [
-  ProductsListComponent
+  ProductsListComponent,
+  ProductDeteailComponent
 ]
 
 @NgModule({
-  declarations: [
-    ...components
-  ],
+  declarations: [...components],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AngularMaterialModule,
     ProductManagementRoutingModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    NgSlimScrollModule,
+    InfiniteScrollModule,
+    NgxMaskModule.forRoot()
   ],
   exports:[...components]
 })
