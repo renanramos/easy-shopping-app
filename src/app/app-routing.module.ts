1eabcf57c7d77833b4bc817b24e37e34992b2668
Renan Ramos <renan.ramos@inatel.br>
2020-10-04 19:20:30 -0300

    Alterados compnentes auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
     {
-        path: '', 
-        loadChildren: () => import('./scaffold/scaffold.module').then(m => m.ScaffoldModule) 
+      path: '', 
+      loadChildren: () => import('./scaffold/scaffold.module').then(m => m.ScaffoldModule) 
     },
     {
       path: 'main',
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 import { AuthGuard } from '../core/guard/auth.guard';
+import { AppAuthGuard } from '../core/guard/app-auth-guard.guard';
 
 const routes: Routes = [
   {
@@ -11,42 +12,42 @@ const routes: Routes = [
       {
         path: 'customer-management',
         loadChildren: () => import('./customer-management/customer-management.module').then(m => m.CustomerManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'company-management',
         loadChildren: () => import('./company-management/company-management.module').then(m => m.CompanyManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'store-management',
         loadChildren: () => import('./store-management/store-management.module').then(m => m.StoreManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'user-management',
         loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'product-management',
         loadChildren: () => import('./product-management/product-management.module').then(m => m.ProductManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'product-category-management',
         loadChildren: () => import('./product-category-management/product-category-management.module').then(m => m.ProductCategoryManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'product-subcategory-management',
         loadChildren: () => import('./subcategory-management/subcategory-management.module').then(m => m.SubcategoryManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'admin-management',
         loadChildren: () => import('./admin-management/admin-management.module').then(m => m.AdminManagementModule),
-        canActivate: [AuthGuard]
+        canActivate: [AppAuthGuard]
       },
       {
         path: 'registration',
