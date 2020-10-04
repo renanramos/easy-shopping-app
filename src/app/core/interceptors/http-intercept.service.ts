5df8c714fc80570504568a746db47a9cfe1e9fd3
Renan Ramos <renan.ramos@inatel.br>
2020-10-04 19:22:09 -0300

    Inicio de implementacao de interceptor com keycloak

--- src/app/core/interceptors/http-intercept.service.ts
+++ src/app/core/interceptors/http-intercept.service.ts
@@ -1,27 +1,33 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpErrorResponse, HttpXsrfTokenExtractor } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { share, tap } from 'rxjs/operators';
 import { SnackbarService } from '../shared/service/snackbar.service';
 import { SecurityUserService } from '../service/auth/security-user.service';
 import { UtilsService } from '../shared/utils/utils.service';
+import { KeycloakService } from 'keycloak-angular';
+import { EasyShoppingKeycloakService } from '../service/auth/easy-shopping-keycloak.service'
 
 @Injectable()
 export class HttpIntercept implements HttpInterceptor {
 
+  authToken: string;
+
   constructor(
+    private easyKeycloakService: EasyShoppingKeycloakService,
+    private keycloakService: KeycloakService,
+    private tokenExtractor: HttpXsrfTokenExtractor,
     private securityUserService: SecurityUserService,
     private utilsService: UtilsService,
     private snackBar: SnackbarService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let headers = new HttpHeaders();
+    this.getTokenFormService();
 
     if (req.url.indexOf('/assets/') < 0) {
-      if (!!this.securityUserService.isUserLogged()) {
-        const token = this.securityUserService.getUserLoggedToken();
-        headers = headers.set('Authorization', `Bearer ${token}`);
+      if (this.securityUserService.idUserLoggedIn) {
+        headers = headers.set('Authorization', `Bearer ${this.authToken}`);
       }
     }
 
@@ -29,23 +35,48 @@ export class HttpIntercept implements HttpInterceptor {
       headers: headers
     });
 
-    const sendRequest = {
-      next: () => {},
-      error: (error: any) => {
-        if (error instanceof HttpErrorResponse) {
-          if (error.status === 401) {
-            this.securityUserService.deleteCookieAndRedirect();
-            const errorMessage = this.utilsService.handleErrorMessage(error);
-            this.snackBar.openSnackBar(errorMessage, 'close');
-          }
-          if (error.status === 0) {
-            this.securityUserService.deleteCookieAndRedirect();
-          }
-        }
-      }
-    }
-
-    return next.handle(authReq)
-      .pipe(tap(sendRequest));
+    return next.handle(authReq);
   }
+
+  async getTokenFormService() {
+    await this.keycloakService.getToken()
+      .then((token) => {
+        this.authToken = token;
+      })
+      .catch(() => this.authToken = '');
+  }
+
+  // intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   let headers = new HttpHeaders();
+
+  //   if (req.url.indexOf('/assets/') < 0) {
+  //     if (!!this.securityUserService.isUserLogged()) {
+  //       const token = this.securityUserService.getUserLoggedToken();
+  //       headers = headers.set('Authorization', `Bearer ${token}`);
+  //     }
+  //   }
+
+  //   const authReq = req.clone({
+  //     headers: headers
+  //   });
+
+  //   const sendRequest = {
+  //     next: () => {},
+  //     error: (error: any) => {
+  //       if (error instanceof HttpErrorResponse) {
+  //         if (error.status === 401) {
+  //           this.securityUserService.deleteCookieAndRedirect();
+  //           const errorMessage = this.utilsService.handleErrorMessage(error);
+  //           this.snackBar.openSnackBar(errorMessage, 'close');
+  //         }
+  //         if (error.status === 0) {
+  //           this.securityUserService.deleteCookieAndRedirect();
+  //         }
+  //       }
+  //     }
+  //   }
+
+  //   return next.handle(authReq)
+  //     .pipe(tap(sendRequest));
+  // }
 }
