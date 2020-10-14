dafe3f93f3ad8373c79060f4f057e6d14e2a2b6a
Renan Ramos <renan.ramos@inatel.br>
2020-10-13 21:29:18 -0300

    Alterado arquivo intercept para incluir token nas requisicoes

--- src/app/core/interceptors/http-intercept.service.ts
+++ src/app/core/interceptors/http-intercept.service.ts
@@ -9,30 +9,45 @@ import { KeycloakService } from 'keycloak-angular';
 import { switchMap, tap } from 'rxjs/operators';
 import { from, pipe } from 'rxjs';
 import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpIntercept implements HttpInterceptor {
 
   constructor(
+    private router: Router,
     private oauthService: OAuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {   
-    let headers = new HttpHeaders();
 
-    headers.set('Authorization', `Bearer ${this.oauthService.getAccessToken()}`);
+    const token = this.gettingAccessTokenFromOauthService();
 
-    const authReq = req.clone({
-      headers: headers
-    });
+    if (token) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      });
 
-    const sendRequest = {
-      next: () => {},
-      error: (error) => {
-        console.log(error);
-      }
-    };
+      const sendRequest = {
+        next: () => {},
+        error: (error) => {
+          console.log(error);
+          if (error) {
+            this.oauthService.logOut();
+            this.router.navigateByUrl('/');
+          }
+        }
+      }; 
 
-    return next.handle(authReq)
-      .pipe(tap(sendRequest));
+      return next.handle(authReq)
+        .pipe(tap(sendRequest));
+    }
+
+    return next.handle(req);
+  }
+
+  gettingAccessTokenFromOauthService() {
+    return this.oauthService.getAccessToken() ? 
+            this.oauthService.getAccessToken() :
+            null;
   }
 }
