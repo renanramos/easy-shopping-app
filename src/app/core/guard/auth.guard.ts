4394639b179fe01e5f2cf41f4eb2b26b8d2719ff
Renan Ramos <renan.ramos@inatel.br>
2020-11-05 18:55:00 -0300

    Alterado redirecionamento em caso de usuario nao logado

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { SecurityUserService } from '../service/auth/security-user.service';
 import { Observable } from 'rxjs';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
   constructor(private securityUserService: SecurityUserService,
-              private router: Router) { }
+              private router: Router,
+              private oauthService: OAuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
     let isUserLogged = this.securityUserService.isUserLogged();
 
     if (!isUserLogged) {
-      this.router.navigate(['/']);
+      this.oauthService.initLoginFlow();
       return false;
     }
     return true;
