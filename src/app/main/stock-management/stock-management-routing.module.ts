9751f4905f99e178ae07c75eea98c1ae2c0c71f0
Renan Ramos <renan.ramos@inatel.br>
2020-11-11 07:58:36 -0300

    Nova rota para stock-items

--- src/app/main/stock-management/stock-management-routing.module.ts
+++ src/app/main/stock-management/stock-management-routing.module.ts
@@ -1,12 +1,19 @@
 import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/core/guard/auth.guard';
 import { StockListComponent } from './stock-list/stock-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: StockListComponent
+    component: StockListComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'items',
+    loadChildren: () => import('./stock-item-management/stock-item-management.module').then(m => m.StockItemManagementModule),
+    canActivate: [AuthGuard]
   }
 ]
 
