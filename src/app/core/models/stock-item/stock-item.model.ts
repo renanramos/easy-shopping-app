5c926bbbdd5b95b02b3e5923f354c07ab355ddd1
Renan Ramos <renan.ramos@inatel.br>
2020-11-11 07:34:07 -0300

    Modelo StockItem

---
+++ src/app/core/models/stock-item/stock-item.model.ts
@@ -0,0 +1,10 @@
+import { Stock } from '../stock/stock.model';
+
+export class StockItem {
+  public id: number;
+  public productId: number;
+  public maxAmount: number;
+  public minAmount: number;
+  public currentAmount: number;
+  public stockId: number;
+}
