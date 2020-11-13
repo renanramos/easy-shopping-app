3eaf3a0af5113577edd179a8fed42802cb7e568a
Renan Ramos <renan.ramos@inatel.br>
2020-11-13 00:02:44 -0300

    Validacao de valores entre minimo e maximo

---
+++ src/app/core/shared/validators/amount-validator.ts
@@ -0,0 +1,26 @@
+import { FormGroup } from '@angular/forms';
+
+export function amountValidator(min: string, max: string) {
+  return (form: FormGroup) => {
+    const minControl = form.controls[min];
+    const maxControl = form.controls[max];
+
+    if (minControl && maxControl) {
+      let minErrors = {...minControl.errors};
+      let maxErrors = {...maxControl.errors};
+
+      if (Number(minControl.value) && Number(minControl.value) > Number(maxControl.value)) {
+        minErrors['invalidValue'] = true;
+        minControl.markAsTouched();
+      } else {
+        if (Object.keys(minErrors).length) {
+          delete minErrors['invalidValue'];
+        }
+      }
+
+      minErrors = Object.keys(minErrors).length ? minErrors : null;
+
+      minControl.setErrors(minErrors);
+    }
+  }
+}
