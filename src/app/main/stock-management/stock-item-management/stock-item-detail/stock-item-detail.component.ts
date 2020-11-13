690bd3c4401fdc0a41084ccc3a0f5e6dd2f3f818
Renan Ramos <renan.ramos@inatel.br>
2020-11-13 00:01:58 -0300

    Implementado metodos de atualizacao e cadastro de itens

--- src/app/main/stock-management/stock-item-management/stock-item-detail/stock-item-detail.component.ts
+++ src/app/main/stock-management/stock-item-management/stock-item-detail/stock-item-detail.component.ts
@@ -4,37 +4,47 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { tap } from 'rxjs/operators';
 import { Product } from 'src/app/core/models/product/product.model';
 import { StockItem } from 'src/app/core/models/stock-item/stock-item.model';
+import { Stock } from 'src/app/core/models/stock/stock.model';
 import { SecurityUserService } from 'src/app/core/service/auth/security-user.service';
 import { ProductService } from 'src/app/core/service/product/product.service';
 import { StockItemService } from 'src/app/core/service/stock-item/stock-item.service';
+import { StockService } from 'src/app/core/service/stock/stock.service';
 import { SnackbarService } from 'src/app/core/shared/service/snackbar.service';
 import { UtilsService } from 'src/app/core/shared/utils/utils.service';
+import { amountValidator } from 'src/app/core/shared/validators/amount-validator';
 
 @Component({
   selector: 'es-stock-item-detail',
   templateUrl: './stock-item-detail.component.html',
   styleUrls: ['./stock-item-detail.component.css'],
-  providers: [StockItemService, ProductService]
+  providers: [StockItemService, ProductService, StockService]
 })
 export class StockItemDetailComponent implements OnInit {
 
   stockItem: StockItem;
   storeId: number = null;
+  stockId: number = null;
   products: Product[] = [];
   stockItemForm: FormGroup;
 
   constructor(private utilsService: UtilsService,
     private formBuilder: FormBuilder,
     private snackBarService: SnackbarService,
-    private securityUserService: SecurityUserService,
     private dialogRef: MatDialogRef<StockItemDetailComponent>,
     private productService: ProductService,
+    private stockItemService: StockItemService,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   async ngOnInit() {
-    this.stockItem = this.data['stockItem'] ? this.data['stockItem'] : new StockItem();
+    this.setStockItemComponentProperties();
     this.createForm();
     await this.loadProductsByStoreId();
+  }
+
+  setStockItemComponentProperties() {
+    this.stockItem = this.data['stockItem'] ? this.data['stockItem'] : new StockItem();
+    this.storeId = this.stockItem['storeId'];
+    this.stockId = this.stockItem['stockId'];
   }
 
   createForm() {
@@ -43,6 +53,9 @@ export class StockItemDetailComponent implements OnInit {
       maxAmount: [this.stockItem['maxAmount'], [Validators.required]],
       minAmount: [this.stockItem['minAmount'], [Validators.required]],
       currentAmount: [this.stockItem['currentAmount'], [Validators.required]]
+    },
+    {
+      validators: amountValidator('minAmount', 'maxAmount')
     })
   }
 
@@ -53,7 +66,7 @@ export class StockItemDetailComponent implements OnInit {
           this.products = products;
         } else {
           this.productId.setErrors({
-            'productsNotFoun': true
+            'productsNotFound': true
           })
           this.productId.markAsTouched();
         }
@@ -87,7 +100,63 @@ export class StockItemDetailComponent implements OnInit {
     return this.stockItemForm.get('currentAmount');
   }
 
-  submitStockItem() {
-    console.log(this.stockItemForm.getRawValue());
+  async submitStockItem() {
+    this.stockItemForm.valid ?
+     await this.handleStockItemOperation() :
+      this.stockItemForm.markAllAsTouched();
+  }
+
+  async handleStockItemOperation() {
+    const stockItem: StockItem = this.getStockItemValuesUpdated();
+    this.stockItem['id'] ?
+      await this.updateStockItem(stockItem) :
+      await this.saveNewStockItem(stockItem) ;
+  }
+
+  async saveNewStockItem(stockItem: StockItem) {
+    const stockItemCreated = {
+      next: (stockItem: StockItem) => {
+        this.dialogRef.close(stockItem);
+      },
+      error: (response) => {
+        const errorMessage = this.utilsService.handleErrorMessage(response);
+        this.snackBarService.openSnackBar(errorMessage);
+      }
+    };
+
+    await this.stockItemService.saveStockItems(stockItem)
+      .pipe(tap(stockItemCreated))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  async updateStockItem(stockItem: StockItem) {
+    stockItem['id'] = this.stockItem['id'];
+    const stockItemCreated = {
+      next: (stockItem: StockItem) => {
+        this.dialogRef.close(stockItem);
+      },
+      error: (response) => {
+        const errorMessage = this.utilsService.handleErrorMessage(response);
+        this.snackBarService.openSnackBar(errorMessage);
+      }
+    };
+
+    await this.stockItemService.updateStockItem(stockItem)
+      .pipe(tap(stockItemCreated))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false); 
+  }
+
+  getStockItemValuesUpdated(): StockItem {
+    const stockItem: StockItem = this.stockItemForm.getRawValue();
+    stockItem['maxAmount'] = Number(this.maxAmount.value);
+    stockItem['minAmount'] = Number(this.minAmount.value);
+    stockItem['currentAmount'] = Number(this.currentAmount.value);
+    stockItem['stockId'] = this.stockId;
+    stockItem['productName'] = this.products.find(prod => this.productId.value === prod['id'])['name'];
+    return stockItem;
   }
 }
