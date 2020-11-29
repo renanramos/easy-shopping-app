b0caf779fdaa1f5a96de8abedd753e928aa7c2c3
Renan Ramos <renan.ramos@inatel.br>
2020-11-28 21:28:48 -0300

    Model de OrderItem

---
+++ src/app/core/models/orderItem/order-item.model.ts
@@ -0,0 +1,9 @@
+export class OrderItem {
+  public id?: number;
+  public orderId: number;
+  public productId: number;
+  public productName: string;
+  public amount?: number;
+  public price: number;
+  public total: number;
+}
