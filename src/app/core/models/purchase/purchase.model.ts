2fefe3d7a089acc1a935f4e6b7639ea082b254f3
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2020-12-05 23:02:37 -0300

    Adicionado campo date

--- src/app/core/models/purchase/purchase.model.ts
+++ src/app/core/models/purchase/purchase.model.ts
@@ -1,6 +1,11 @@
+import { Address } from '../address/address.model';
+import { CreditCard } from '../credit-card/credit-card.model';
+import { Order } from '../order/order.model';
+
 export class Purchase {
   public id?: number;
   public orderId: number;
   public addressId: number;
   public creditCardId: number;
+  public date?: any;
 }
