7a3a20dcaeb5d32774b8d8b06ad763522b09b129
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2020-12-09 21:45:07 -0300

    Modelo purchaseStatistic

---
+++ src/app/core/models/purchase/purchase-statistic.model.ts
@@ -0,0 +1,7 @@
+import { Order } from '../order/order.model';
+import { Purchase } from './purchase.model';
+
+export class PurchaseStatistic {
+    public order: Order;
+    public purchase: Purchase; 
+}
