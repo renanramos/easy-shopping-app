9015022c68ad5fbbd2da9f44f7d126aff5191a5c
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2020-12-12 19:24:06 -0300

    Nova propriedade no arquivo de constants

--- src/app/core/shared/constants/auth-constants.ts
+++ src/app/core/shared/constants/auth-constants.ts
@@ -3,4 +3,5 @@ export class AuthConstants {
   public static AUTH_TOKEN: string = 'AUTH_TOKEN';
   public static USERNAME: string = 'USER_NAME';
   public static USER_ROLE: string = 'USER_ROLE';
+  public static SYNC: string = 'SYNC';
 }
