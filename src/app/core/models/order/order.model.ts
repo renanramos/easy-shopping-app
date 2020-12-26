8afd2c1047d3df56f76e0003e5d78c27b8d8207d
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2020-12-26 18:13:01 -0300

    Propriedade items adicionado ao model Order

--- src/app/core/models/order/order.model.ts
+++ src/app/core/models/order/order.model.ts
@@ -1,6 +1,9 @@
+import { OrderItem } from "../orderItem/order-item.model";
+
 export class Order {
   public id?: number;
   public orderNumber: string;
   public customerId: string;
   public finished?: boolean;
+  public items?: OrderItem[];
 }
