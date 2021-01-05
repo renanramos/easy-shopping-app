67a768a88a8c2833ceda8c02abe2216aace8a7cf
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-01-04 22:21:19 -0300

    Correcao na exportacao de relatorio .csv

--- src/app/core/models/export-csv/export-csv-model.ts
+++ src/app/core/models/export-csv/export-csv-model.ts
@@ -1,11 +1,11 @@
 export class ExportCsvOptions {
-  public fieldSeparator: string = ';';
+  public fieldSeparator: string = ',';
   public quoteStrings: string = '"';
   public decimalSeparator: string = '.';
   public showLabels: boolean = true;
   public showTitle: boolean = true;
   public title: string = '';
-  public useTextFile: boolean = true;
+  public useTextFile: boolean = false;
   public useBom: boolean = true;
   public useKeysAsHeaders: boolean = false;
   public headers: string[] = [];
